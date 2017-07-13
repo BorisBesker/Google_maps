fe529362f373d421a565e77e4d332bf9bde6d5e4
Boris Besker <borisbesker@Boriss-MacBook-Pro.local>
2017-07-13 19:05:25 +0200

    Added registration feature

--- Google_maps/urls.py
+++ Google_maps/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^$', views.Login.as_view(), name="login"),
     url(r'^admin/', admin.site.urls),
     url(r'^logout/$', views.Logout.as_view(), name="logout"),
+    url(r'^register/$', views.Register.as_view(), name="register"),
     url(r'^welcome/$', views.Welcome.as_view(), name="welcome")
 ]
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate, get_user_model
+
+
+User = get_user_model()
 
 
 class UserForm(forms.Form):
@@ -12,5 +15,31 @@ class UserForm(forms.Form):
             raise forms.ValidationError('Invalid credentials')
         elif not user.is_active:
             raise forms.ValidationError('Disabled account')
-
         return super(UserForm, self).clean(*args, **kwargs)
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label='Confirm password')
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean(self, *args, **kwargs):
+        if self.cleaned_data['password'] != self.cleaned_data['password1']:
+            raise forms.ValidationError('Passwords must match')
+        return super(RegisterForm, self).clean(*args, **kwargs)
+
+    def clean_email(self, *args, **kwargs):
+        email_qs = User.objects.filter(email=self.cleaned_data['email'])
+        if email_qs.exists():
+            raise forms.ValidationError('This email has already been registered')
+        return self.cleaned_data['email']
+
+    def clean_username(self, *args, **kwargs):
+        user_qs = User.objects.filter(username=self.cleaned_data['username'])
+        if user_qs.exists():
+            raise forms.ValidationError('This username already exists')
+        return self.cleaned_data['username']
