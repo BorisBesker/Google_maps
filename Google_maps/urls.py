ab6e8c0a2e0fce922e42d0b9a17c9a7518b25da0
Boris Besker <borisbesker@Boriss-MacBook-Pro.local>
2017-09-13 19:32:37 +0200

    Dummy commit

--- Google_maps/urls.py
+++ Google_maps/urls.py
@@ -1,12 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from allauth.account.views import LoginView
 
 from accounts import views
 
 urlpatterns = [
-    url(r'^$', views.Login.as_view(), name="login"),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', LoginView.as_view(), name="z"),
+
     url(r'^logout/$', views.Logout.as_view(), name="logout"),
     url(r'^register/$', views.Register.as_view(), name="register"),
-    url(r'^welcome/$', views.Welcome.as_view(), name="welcome")
+    url(r'^welcome/$', views.Welcome.as_view(), name="welcome"),
+    url(r'^accounts/', include('allauth.urls')),
 ]
