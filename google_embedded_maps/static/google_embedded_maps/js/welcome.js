0e7abf2688ade55ad8aeb627e382a141d865a107
Boris Besker <borisbesker@Boriss-MacBook-Pro.local>
2017-10-02 10:16:40 +0200

    Added search mode

--- google_embedded_maps/static/google_embedded_maps/js/welcome.js
+++ google_embedded_maps/static/google_embedded_maps/js/welcome.js
@@ -5,6 +5,22 @@ $(document).ready(function() {
         $('textarea').val(text);
     });
 
+
+    // Added function for changing the mode on the map
+
+    $("select").change(function () {
+        var search_mode = $( "select option:selected" ).val();
+        var src_iframe = $("iframe").attr("src");
+        if (search_mode === 'Place mode') {
+            src_iframe = src_iframe.replace("search", "place");
+            $("iframe").attr("src", src_iframe);
+        }
+        if (search_mode === 'Search mode') {
+            src_iframe = src_iframe.replace("place", "search");
+            $("iframe").attr("src", src_iframe);
+        }
+    });
+
     $("#copyButton").click(function() {
         $('textarea').select();
         document.execCommand('copy');
@@ -16,11 +32,14 @@ $(document).ready(function() {
         }
     });
 
+
+    // Modified function for changing the location
+
     $("#searchButton").click(function() {
         var place = $("#locationTextField").val();
-        var src_iframe = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyDY2kqCa1Yk3_9xOICSwiX24W1avKFpvn0&q=';
-
-        src_iframe = src_iframe + place;
+        var src_iframe = $("iframe").attr("src");
+        get_location = src_iframe.match(/q=(.+)/);
+        src_iframe = src_iframe.replace(get_location[1], place);
         $("iframe").attr("src", src_iframe);
     });
 
